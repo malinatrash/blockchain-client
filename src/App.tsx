cfa42a3a426f2116ccc85c914829cb0359a3240e
Pavel Naumov <malinatrashh@gmail.com>
2024-03-12 17:11:46 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+	const [data, setData] = useState(null)
+
+	useEffect(() => {
+		fetchData()
+	}, [])
+
+	const fetchData = async () => {
+		try {
+			const response = await fetch('http://localhost:8080/your_endpoint')
+			const jsonData = await response.json()
+			setData(jsonData)
+		} catch (error) {
+			console.error('Error fetching data:', error)
+		}
+	}
+
+	return (
+		<div className='App'>
+			<h1>JSON Data:</h1>
+			{data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
+		</div>
+	)
+}
+
+export default App
